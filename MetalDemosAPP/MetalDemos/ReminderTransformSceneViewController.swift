ec0059124f36bcb0dfde4eb1a29ca2ec273b06d7
Ewan <houqingchen@hotmail.com>
2021-06-18 00:12:26 +0800

    + add an iOS app for showing my all kinds of metal demos

---
+++ MetalDemosAPP/MetalDemos/ReminderTransformSceneViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderTransformSceneViewcontroller.swift
+//  Today
+//
+//  Created by ThisEwan on 2021/6/17.
+//
+
+import UIKit
+
+class ReminderTransformSceneViewController: UIViewController {
+    override func viewDidLoad() {
+        NSLog("configue")
+    }
+    
+    func configue() {
+        NSLog("configue")
+    }
+}
