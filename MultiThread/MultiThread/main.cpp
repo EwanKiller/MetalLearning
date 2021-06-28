366d7d9d0a122d58ee73b07125679b183767b371
Ewan <houqingchen@hotmail.com>
2021-06-29 00:04:15 +0800

    add multi thread learn project!

---
+++ MultiThread/MultiThread/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MultiThread
+//
+//  Created by ThisEwan on 2021/6/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
